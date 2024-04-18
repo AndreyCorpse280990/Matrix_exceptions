0ec4dfa2ecd08208e5705beac2c28d0b005bf6cd
AndreyCorpse28090 <280990corpse@gmail.com>
2024-04-18 06:30:51 +0300

    1

---
+++ Matrix_exceptions/Matrix.cs
@@ -0,0 +1,49 @@
+﻿namespace Matrix_exceptions;
+
+// Данный класс реализует функционал математического объекта «матрица». 
+public class Matrix
+{
+    private int Cols { get; set; }
+    public int Rows { get; set;}
+    private int[,] array;
+    private Random random = new Random();
+    
+
+            
+    // Конструктор класса.
+    public Matrix() {}
+
+    // Конструктор класса для инициализации матрицы заданными значениями.
+    public Matrix(int cols, int rows)
+    {
+        InitializeColsRows(cols, rows);
+    }
+    
+    // Конструктор класса для инициализации матрицы случайными значениями.
+    public Matrix(int cols, int rows, Random random)
+    {
+        InitializeRandom(random);
+    }
+    
+    // метод инициализации матрицы заданым размером.
+    private void InitializeColsRows(int cols, int rows)
+    {
+        this.Cols = cols;
+        this.Rows = rows;
+        array = new int[this.Cols, this.Rows];
+    }
+    
+    // метод инициализации матрицы cлучайными значениями.
+    public void InitializeRandom(Random random)
+    {
+        for (int i = 0; i < this.Cols; i++)
+        {
+            for (int j = 0; j < this.Rows; j++)
+            {
+                array[i, j] = random.Next(100);
+            }
+        }
+    }
+    
+    
+}
---
+++ Matrix_exceptions/Program.cs
@@ -0,0 +1,18 @@
+﻿global using global::System;
+global using global::System.Collections.Generic;
+global using global::System.IO;
+global using global::System.Linq;
+global using global::System.Net.Http;
+global using global::System.Threading;
+global using global::System.Threading.Tasks;
+
+namespace Matrix_exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
