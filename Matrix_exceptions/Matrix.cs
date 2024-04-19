fc05fcaf2ee912831886ee7af915fab40282d720
AndreyCorpse28090 <280990corpse@gmail.com>
2024-04-19 22:52:59 +0300

    done

--- Matrix_exceptions/Matrix.cs
+++ Matrix_exceptions/Matrix.cs
@@ -1,47 +1,253 @@
-﻿namespace Matrix_exceptions;
+﻿using System;
 
-// Данный класс реализует функционал математического объекта «матрица». 
-public class Matrix
+namespace Matrix_exceptions
 {
-    private int Cols { get; set; }
-    public int Rows { get; set;}
-    private int[,] array;
-    private Random random = new Random();
-    
-    // Конструктор класса.
-    public Matrix() {}
+    public class MatrixException : ApplicationException
+    {
+        public MatrixException() : base() { }
+        public MatrixException(string message) : base(message) { }
+        public MatrixException(string message, Exception innerException) : base(message, innerException) { }
+        protected MatrixException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
 
-    // Конструктор класса для инициализации матрицы заданными значениями.
-    public Matrix(int cols, int rows)
+    public class Matrix
     {
-        InitializeColsRows(cols, rows);
-    }
-    
-    // Конструктор класса для инициализации матрицы случайными значениями.
-    public Matrix(int cols, int rows, Random random)
-    {
-        InitializeRandom(random);
-    }
-    
-    // метод инициализации матрицы заданым размером.
-    private void InitializeColsRows(int cols, int rows)
-    {
-        this.Cols = cols;
-        this.Rows = rows;
-        array = new int[this.Cols, this.Rows];
-    }
-    
-    // метод инициализации матрицы cлучайными значениями.
-    public void InitializeRandom(Random random)
-    {
-        for (int i = 0; i < this.Cols; i++)
+        private int Cols { get; set; }
+        public int Rows { get; set; }
+        private int[,] array;
+        private Random random = new Random();
+
+        // Конструктор класса.
+        public Matrix() { }
+
+        // Конструктор класса для инициализации матрицы заданным размером.
+        public Matrix(int cols, int rows)
         {
-            for (int j = 0; j < this.Rows; j++)
+            try
             {
-                array[i, j] = random.Next(100);
+                InitializeColsRows(cols, rows);
+            }
+            catch (MatrixException ex)
+            {
+                Console.WriteLine($"Ошибка при создании матрицы: {ex.Message}");
+            }
+        }
+
+        // Конструктор класса для инициализации матрицы случайными значениями.
+        public Matrix(int cols, int rows, Random random)
+        {
+            try
+            {
+                InitializeColsRows(cols, rows); // инициализация матрицы
+                InitializeRandom(random);
+            }
+            catch (MatrixException ex)
+            {
+                Console.WriteLine($"Ошибка при создании матрицы: {ex.Message}");
+            }
+
+        }
+
+        // метод инициализации матрицы заданым размером.
+        private void InitializeColsRows(int cols, int rows)
+        {
+            try
+            {
+                if (cols <= 0 || rows <= 0)
+                {
+                    throw new MatrixException("Размер матрицы должен быть положительным числом.");
+                }
+
+                this.Cols = cols;
+                this.Rows = rows;
+                array = new int[this.Cols, this.Rows];
+            }
+            catch (Exception ex)
+            {
+                throw new MatrixException("Ошибка при инициализации матрицы.", ex);
+            }
+        }
+
+        // метод инициализации матрицы случайными значениями.
+        public void InitializeRandom(Random random)
+        {
+            try
+            {
+                if (random == null)
+                {
+                    throw new MatrixException("Ссылка на объект Random не указывает на экземпляр.");
+                }
+                for (int i = 0; i < this.Cols; i++)
+                {
+                    for (int j = 0; j < this.Rows; j++)
+                    {
+                        array[i, j] = random.Next(100);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new MatrixException("Ошибка при инициализации матрицы случайными значениями.", ex);
+            }
+        }
+
+        // Получение элемента матрицы.
+        public int GetElement(int col, int row)
+        {
+            try
+            {
+                if (col < 0 || col >= Cols || row < 0 || row >= Rows)
+                {
+                    throw new MatrixException("Индексы выходят за границы матрицы.");
+                }
+                return array[col, row];
+            }
+            catch (Exception ex)
+            {
+                throw new MatrixException("Ошибка при получении элемента матрицы.", ex);
+            }
+        }
+
+        // Установка значения элемента матрицы.
+        public void SetElement(int col, int row, int value)
+        {
+            try
+            {
+                if (col < 0 || col >= Cols || row < 0 || row >= Rows)
+                {
+                    throw new MatrixException("Индексы выходят за границы матрицы.");
+                }
+                array[col, row] = value;
+            }
+            catch (Exception ex)
+            {
+                throw new MatrixException("Ошибка при установке значения элемента матрицы.", ex);
+            }
+        }
+
+        // Печать матрицы   
+        public void PrintMatrix()
+        {
+            try
+            {
+                for (int i = 0; i < Rows; i++)
+                {
+                    for (int j = 0; j < Cols; j++)
+                    {
+                        Console.Write(array[i, j] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new MatrixException("Ошибка при печати матрицы.", ex);
+            }
+        }
+
+        // Сложение матриц.
+        public static Matrix Addition(Matrix a, Matrix b)
+        {
+            try
+            {
+                if (a.Cols != b.Cols || a.Rows != b.Rows)
+                {
+                    throw new MatrixException("Невозможно сложить матрицы разной размерности.");
+                }
+                Matrix result = new Matrix(a.Cols, a.Rows);
+                for (int i = 0; i < a.Cols; i++)
+                {
+                    for (int j = 0; j < a.Rows; j++)
+                    {
+                        result.array[i, j] = a.array[i, j] + b.array[i, j];
+                    }
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new MatrixException("Ошибка при сложении матриц.", ex);
+            }
+        }
+
+        // Вычитание матриц.
+        public static Matrix Subtraction(Matrix a, Matrix b)
+        {
+            try
+            {
+                if (a.Cols != b.Cols || a.Rows != b.Rows)
+                {
+                    throw new MatrixException("Невозможно вычесть матрицы разной размерности.");
+                }
+                Matrix result = new Matrix(a.Cols, a.Rows);
+                for (int i = 0; i < a.Cols; i++)
+                {
+                    for (int j = 0; j < a.Rows; j++)
+                    {
+                        result.array[i, j] = a.array[i, j] - b.array[i, j];
+                    }
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new MatrixException("Ошибка при вычитании матриц.", ex);
+            }
+        }
+        
+        // Умножение матриц.
+        public static Matrix Multiplication(Matrix a, Matrix b)
+        {
+            try
+            {
+                if (a.Cols != b.Rows)
+                {
+                    throw new MatrixException("Невозможно умножить матрицы с данными размерностями.");
+                }
+                Matrix result = new Matrix(b.Cols, a.Rows);
+
+                for (int i = 0; i < result.Rows; i++)
+                {
+                    for (int j = 0; j < result.Cols; j++)
+                    {
+                        int sum = 0;
+                        for (int k = 0; k < a.Cols; k++)
+                        {
+                            sum += a.array[i, k] * b.array[k, j];
+                        }
+                        result.array[i, j] = sum;
+                    }
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new MatrixException("Ошибка при умножении матриц.", ex);
+            }
+        }
+        
+        // запись в файл
+        public void WriteToFile(Matrix matrix, string fileName)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(fileName))
+                {
+                    for (int i = 0; i < Rows; i++)
+                    {
+                        for (int j = 0; j < Cols; j++)
+                        {
+                            writer.Write(array[i, j] + "\t");
+                        }
+                        writer.WriteLine();
+                    }
+                }
+                Console.WriteLine("Матрица успешно записана в файл.");
+            }
+            catch (Exception ex)
+            {
+                throw new MatrixException($"Ошибка при записи матрицы в файл: {ex.Message}", ex);
             }
         }
     }
-    
-    
 }
--- Matrix_exceptions/Program.cs
+++ Matrix_exceptions/Program.cs
@@ -5,6 +5,8 @@ global using global::System.Linq;
 global using global::System.Net.Http;
 global using global::System.Threading;
 global using global::System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Threading.Channels;
 
 namespace Matrix_exceptions
 {
@@ -12,7 +14,28 @@ namespace Matrix_exceptions
     {
         static void Main(string[] args)
         {
+            string path = "E:\\programming\\Visual Studio\\repos\\Matrix_exceptions\\matrix.txt";
+            Random random = new Random();
+            Matrix matrix1 = new Matrix(5, 5, random);//Создание matrix1  со случайными значениями
+            Matrix matrix2 = new Matrix(5, 5, random); // Создание matrix2  со случайными значениями
             
+            Console.WriteLine("Матрица 1:");
+            matrix1.PrintMatrix();
+            Console.WriteLine("\nМатрица 2:");
+            matrix2.PrintMatrix();
+
+            Matrix sum = Matrix.Addition(matrix1, matrix2);
+            Console.WriteLine($"\nСумма матриц 1 и 2: ");
+            sum.PrintMatrix();
+
+            Matrix sub = Matrix.Subtraction(matrix1, matrix2);
+            Console.WriteLine("\nРазность матриц 1 и 2: ");
+            sub.PrintMatrix();
+            
+            Matrix mul = Matrix.Multiplication(matrix1, matrix2);
+            Console.WriteLine("\nПроизведение матриц 1 и 2: ");
+            mul.PrintMatrix();
+            mul.WriteToFile(mul, path); // Запись в файл
         }
     }
 }
